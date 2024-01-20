479369b5cd888a7de18edf1403ffd19546e2bbd7
rickyreza12 <muhammadrickyreza@gmail.com>
2024-01-21 01:09:25 +0700

    change main.py

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
+@app.route('/')
+def hello():
+    return jsonify({"msg":"test done"})
+
 @app.route('/execute_selenium', methods=['POST'])
 def execute_selenium():
     data = request.get_json()
@@ -49,9 +53,6 @@ def execute_selenium():
     except Exception as e:
         result = {'status': 'error', 'message': str(e)}
         return jsonify(result)
-    # finally:
-    # Uncomment the following line if you want to quit the driver after each request
-    # driver.quit()
 
 @app.route('/change_password', methods=['POST'])
 def change_password():
@@ -76,31 +77,20 @@ def change_password():
 
 def go_to_network(driver, password, new_password):
     try:
-        
-        # apply_button = WebDriverWait(driver, 10).until(
-        #     EC.element_to_be_clickable((By.XPATH, "//input[@type='button' and @onclick='saveApply()']"))
-        # )
-        
-
         # Retrieve data from password input field 
         password_input = driver.find_element("id", "PreSharedKey")
         password_data_before = password_input.get_attribute("value")
         
-        if(password != password_data_before):
-             result = {'status': 'success', 'message': 'the current password are wrong'}
+        if password != password_data_before:
+             result = {'status': 'success', 'message': 'the current password is wrong'}
              return result
         
-
         password_input.send_keys(new_password)
         password_data_after = password_input.get_attribute("value")
 
-        # apply_button.click()
-
-        result = {'status': 'success', 'message': 'Modification successfull', 'password_before': password_data_before, 'password_after': password_data_after}
-        # result = {'status': 'success', 'message': 'Modification successfull'}
+        result = {'status': 'success', 'message': 'Modification successful', 'password_before': password_data_before, 'password_after': password_data_after}
         return result
     except NoSuchElementException as e:
-        # Handle element not found gracefully
         traceback_str = traceback.format_exc()
         result = {'status': 'error', 'message': 'Apply button not found or password input not found', 'traceback': traceback_str}
         return result
